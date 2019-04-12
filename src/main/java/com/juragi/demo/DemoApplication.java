520d3e4c603005d53d113390d7d94da87428d551
juragi <juragi21@hotmail.com>
2019-04-12 22:34:39 +0900

    외부 톰캣에 war 배포하는 방식으로 변경

--- src/main/java/com/juragi/demo/DemoApplication.java
+++ src/main/java/com/juragi/demo/DemoApplication.java
@@ -3,13 +3,21 @@ package com.juragi.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.juragi.m")
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+	/*
+	 * public static void main(String[] args) {
+	 * SpringApplication.run(DemoApplication.class, args); }
+	 */
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
 
 }
